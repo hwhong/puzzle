2c120d9da251f7e1a77512cfb18a9f66c38aec6f
Warren Hong <hwhong1129@gmail.com>
2023-01-31 21:59:18 -0800

    feature: fixes puzzleid page

--- pages/[puzzleId].tsx
+++ pages/[puzzleId].tsx
@@ -1,26 +1,83 @@
+import { ART_WORKS } from "@/utils/helper";
+import Draggable from "@/components/draggable";
+import { Inter } from "@next/font/google";
+import { useEffect, useRef } from "react";
+import styles from "../styles/index.module.css";
 import { useRouter } from "next/router";
-import React from "react";
 
-export default function Puzzle({ message }) {
+const SIZE = 100;
+const X_INCREMENTS = 100;
+const Y_INCREMENTS = 20;
+const TOTAL_PIECES = 25;
+
+export default function Puzzle() {
   const router = useRouter();
+  const itemsRef = useRef<HTMLCanvasElement[]>([]);
+
   const { puzzleId } = router.query;
-  console.log(router.query);
+
+  useEffect(() => {
+    const initItems = () => {
+      if (itemsRef.current[0]) {
+        for (let y = 0; y < 5; y++) {
+          for (let x = 0; x < 5; x++) {
+            const ctx = itemsRef.current[y * 5 + x]?.getContext("2d");
+            if (ctx) {
+              // prettier-ignore
+              ctx.drawImage(
+                image,
+                x * SIZE, y * SIZE, SIZE, SIZE,
+                0, 0, SIZE, SIZE
+              );
+
+              // x is starting, d is destination
+              //drawImage(image,
+              // sx, sy, sw, sh,
+              // dx, dy, dw, dh);
+            }
+          }
+        }
+      }
+    };
+
+    itemsRef.current = itemsRef.current.slice(0, TOTAL_PIECES);
+
+    const image = document.createElement("img");
+    image.setAttribute("src", `/${puzzleId}.png`);
+    image.addEventListener("load", () => {
+      initItems();
+    });
+  }, [itemsRef, puzzleId]);
+
   return (
-    <div>
-      <p>{message}</p>
-      <p>{puzzleId}</p>
+    <div className={styles.root}>
+      {Array.from(Array(TOTAL_PIECES).keys()).map((i) => (
+        <Draggable
+          xIncrements={X_INCREMENTS}
+          yIncrements={Y_INCREMENTS}
+          key={i}
+        >
+          <canvas
+            ref={(el) => {
+              if (itemsRef.current && el) {
+                itemsRef.current[i] = el;
+              }
+            }}
+            width={SIZE}
+            height={SIZE}
+          />
+        </Draggable>
+      ))}
     </div>
   );
 }
 
-export async function getStaticProps(context) {
-  return {
-    props: { message: "dynamic page part 2" }, // will be passed to the page component as props
-  };
+export async function getStaticProps() {
+  return {};
 }
 
 export async function getStaticPaths() {
-  const posts = ["wave", "kiss"];
+  const posts = ART_WORKS;
   const paths = posts.map((post) => ({
     params: { puzzleId: post },
   }));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,8 @@
-import Draggable from "@/components/draggable";
 import { Inter } from "@next/font/google";
-import { useEffect, useRef } from "react";
 import styles from "../styles/index.module.css";
-import img from "../public/wave.png";
-import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
-const SIZE = 100;
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const itemsRef = useRef<HTMLCanvasElement[]>([]);
-  useEffect(() => {
-    const initItems = () => {
-      if (itemsRef.current[0]) {
-        for (let y = 0; y < 5; y++) {
-          for (let x = 0; x < 5; x++) {
-            const ctx = itemsRef.current[y * 5 + x]?.getContext("2d");
-            if (ctx) {
-              // prettier-ignore
-              ctx.drawImage(
-                image,
-                x * SIZE, y * SIZE, SIZE, SIZE,
-                0, 0, SIZE, SIZE
-              );
-
-              // x is starting, d is destination
-              //drawImage(image,
-              // sx, sy, sw, sh,
-              // dx, dy, dw, dh);
-            }
-          }
-        }
-      }
-    };
-
-    const image = document.createElement("img");
-    image.setAttribute("src", "/wave.png");
-    itemsRef.current = itemsRef.current.slice(0, 25);
-    image.src = "/wave.png";
-    image.addEventListener("load", () => {
-      initItems();
-    });
-  }, [itemsRef]);
-
-  return (
-    <div className={styles.root}>
-      {Array.from(Array(25).keys()).map((i) => (
-        <Draggable xIncrements={100} yIncrements={20} key={i}>
-          <canvas
-            ref={(el) => {
-              if (itemsRef.current && el) {
-                itemsRef.current[i] = el;
-              }
-            }}
-            width={100}
-            height={100}
-          />
-        </Draggable>
-      ))}
-      {/* <Draggable xIncrements={100} yIncrements={20} key={"one"}>
-        <canvas ref={canvasRef} width={100} height={100} />
-      </Draggable>
-      <Draggable xIncrements={100} yIncrements={20} key={"two"}>
-        <div className={styles.square}>2</div>
-      </Draggable>
-      <Draggable xIncrements={100} yIncrements={20} key={"three"}>
-        <div className={styles.square}>3</div>
-      </Draggable>
-      <Draggable xIncrements={100} yIncrements={20} key={"four"}>
-        <div className={styles.square}>4</div>
-      </Draggable> */}
-    </div>
-  );
+  return <div className={styles.root}></div>;
 }
